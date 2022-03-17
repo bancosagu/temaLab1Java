e655a646da643970da3e08dbcde9b249634a2f04
Augustin Bancos <bancosagu@gmail.com>
2022-03-17 23:25:16 +0100

    Update 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,46 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello \n" +
-                "world!");
-        //test update git
+                "Augustin");
+        System.out.println("Avem doua numere intregi, si anume 100 si 31");
+            int x, y, sum;
+            x=100;
+            y=31;
+            sum = x+y;
+        System.out.println("Rezultatul adunarii celor doua numere este " + sum);
+            double a, b;
+            a=100;
+            b=31;
+        System.out.println("Rezultatul impartirii celor doua numere este " + a/b);
+
+//        Scanner in= new Scanner(System.in);
+//        System.out.println("Introdu primul numar");
+//        int num1 = in.nextInt();
+//        System.out.println("Introdu al doilea numar");
+//        int num2 = in.nextInt();
+//        System.out.println(num1+ "+" +num2+ "=" +
+//                (num1+num2));
+
+
+            int m=55;
+            int n=9;
+        System.out.println("(55+9)%9="+ (a+b)%b);
+
+//4. Printati rezultatul urmatoarelor operatiuni:
+//
+//a. -5 + 8 * 6 = 43
+//
+//b. (55+9) % 9 = 1
+//
+//c. 20 + -3*5 / 8 =
+//
+//d. 5 + 15 / 3 * 2 - 8 % 3 =13?
+
+
 
     }
 }
