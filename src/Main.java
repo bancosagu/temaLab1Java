64f7b5cad0ec3c2f3961b295ae669d04a2db90b8
Augustin Bancos <bancosagu@gmail.com>
2022-03-19 17:54:54 +0100

    Update 1.2 - aproape gata

--- src/Main.java
+++ src/Main.java
@@ -7,41 +7,33 @@ public class Main {
         System.out.println("Hello \n" +
                 "Augustin");
         System.out.println("Avem doua numere intregi, si anume 100 si 31");
-            int x, y, sum;
-            x=100;
-            y=31;
-            sum = x+y;
+        int x, y, sum;
+        x = 100;
+        y = 31;
+        sum = x + y;
         System.out.println("Rezultatul adunarii celor doua numere este " + sum);
-            double a, b;
-            a=100;
-            b=31;
-        System.out.println("Rezultatul impartirii celor doua numere este " + a/b);
-
-//        Scanner in= new Scanner(System.in);
-//        System.out.println("Introdu primul numar");
-//        int num1 = in.nextInt();
-//        System.out.println("Introdu al doilea numar");
-//        int num2 = in.nextInt();
-//        System.out.println(num1+ "+" +num2+ "=" +
-//                (num1+num2));
+        double a, b;
+        a = 100;
+        b = 31;
+        System.out.println("Rezultatul impartirii celor doua numere este " + a / b);
 
 
-            int m=55;
-            int n=9;
-        System.out.println("(55+9)%9="+ (a+b)%b);
-        int u=125;
 
-//4. Printati rezultatul urmatoarelor operatiuni:
-//
-//a. -5 + 8 * 6 = 43
-//
-//b. (55+9) % 9 = 1
-//
-//c. 20 + -3*5 / 8 =
-//
-//d. 5 + 15 / 3 * 2 - 8 % 3 =13?
+        System.out.println("a.  -5 + 8 * 6 = "+ (int) (-5+8*6));
 
+        System.out.println("b.  (55 + 9) % 9 = " + (double) (55+9) / 9);
 
+        int no1=20;
+        int no2= -3*5/8;
+
+        System.out.println((double)no1+no2);
+
+        System.out.println("c.  20 + -3 * 5 / 8 = " + (double) 20+((-3*5)/8));
+        //sistemul rotunjeste in sus la 19. Rezultatul trebuie sa fie 18,125
+
+        System.out.println("d.  5 + 15 / 3 * 2 - 8 % 3 = " + (int)(5 + 15 / 3 * 2 - 8 % 3) );
 
     }
+
+
 }
